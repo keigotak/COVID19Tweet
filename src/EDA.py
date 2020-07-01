d2d20f637a811b8895a414b9e1768d410db83de5
keigotak <keigotak@localhost.com>
2020-07-02 01:52:44 +0900

    feat: add basic EDA

--- src/EDA.py
+++ src/EDA.py
@@ -1,10 +1,25 @@
 from HelperFunctions import get_raw_datasets
 
 
+def probe_labels():
+    datasets, tags, path = get_raw_datasets()
+    labels = {tag: set() for tag in tags}
+    counts = {tag: {} for tag in tags}
+
+    for tag in tags:
+        for id, text, label in datasets[tag]:
+            labels[tag].add(label)
+            if label in counts[tag].keys():
+                counts[tag][label] += 1
+            else:
+                counts[tag][label] = 1
+    return {'counts': counts, 'labels': labels}
+
+
 def probe_sentence_length():
     datasets, tags, path = get_raw_datasets()
-
     counts = {tag: {} for tag in tags}
+    counts_by_labels = {tag: {'INFORMATIVE': {}, 'UNINFORMATIVE': {}} for tag in tags}
 
     for tag in tags:
         for id, text, label in datasets[tag]:
@@ -13,13 +28,21 @@ def probe_sentence_length():
                 counts[tag][len(words)].append(words)
             else:
                 counts[tag][len(words)] = [words]
-    return counts
 
-def probe_stats():
+            if len(words) in counts_by_labels[tag][label].keys():
+                counts_by_labels[tag][label][len(words)] += 1
+            else:
+                counts_by_labels[tag][label][len(words)] = 1
+
+    return {'counts': counts, 'counts_by_labels': counts_by_labels}
+
+
+def probe_vocabs():
     datasets, tags, path = get_raw_datasets()
-
     vocabs = {tag: set() for tag in tags}
     counts = {tag: {} for tag in tags}
+    vocabs_by_labels = {tag: {'INFORMATIVE': set(), 'UNINFORMATIVE': set()} for tag in tags}
+    counts_by_labels = {tag: {'INFORMATIVE': {}, 'UNINFORMATIVE': {}} for tag in tags}
 
     for tag in tags:
         for id, text, label in datasets[tag]:
@@ -27,38 +50,66 @@ def probe_stats():
             for word in words:
                 word = word.lower()
                 vocabs[tag].add(word)
+                vocabs_by_labels[tag][label].add(word)
                 if word in counts[tag].keys():
                     counts[tag][word] += 1
                 else:
                     counts[tag][word] = 1
-        print(counts[tag])
-    return counts, vocabs
+                if word in counts_by_labels[tag][label].keys():
+                    counts_by_labels[tag][label][word] += 1
+                else:
+                    counts_by_labels[tag][label][word] = 1
+    return {'counts': counts, 'vocabs': vocabs, 'counts_by_labels': counts_by_labels, 'vocab_by_labels': vocabs_by_labels}
 
 
 def generate_stats_files():
-    counts, vocabs = probe_stats()
-    for tag in counts.keys():
-        sorted_counts = sorted(counts[tag].items(), key=lambda x: x[1], reverse=True)
-        with open('../data/analytic/counts-{}.csv'.format(tag), 'w', encoding='utf-8-sig') as f:
+    rets = probe_labels()
+    for tag in rets['counts'].keys():
+        with open('../data/analytic/counts-label-{}.csv'.format(tag), 'w', encoding='utf-8-sig') as f:
+            f.write('label,frequency\n')
+            for key, val in rets['counts'][tag].items():
+                f.write('{},{}\n'.format(key, val))
+    labels = rets['labels']
+
+    rets = probe_vocabs()
+    for tag in rets['counts'].keys():
+        sorted_counts = sorted(rets['counts'][tag].items(), key=lambda x: x[1], reverse=True)
+        with open('../data/analytic/counts-vocab-{}.csv'.format(tag), 'w', encoding='utf-8-sig') as f:
+            f.write('word,frequency\n')
             for key, val in sorted_counts:
                 f.write('{},{}\n'.format(key, val))
 
-    counts = probe_sentence_length()
-    for tag in counts.keys():
-        max_len = max(counts[tag].keys())
-        min_len = min(counts[tag].keys())
+        for label in labels[tag]:
+            sorted_counts = sorted(rets['counts_by_labels'][tag][label].items(), key=lambda x: x[1], reverse=True)
+            with open('../data/analytic/counts-vocab-{}-{}.csv'.format(tag, label), 'w', encoding='utf-8-sig') as f:
+                f.write('word,frequency\n')
+                for key, val in sorted_counts:
+                    f.write('{},{}\n'.format(key, val))
+
+    rets = probe_sentence_length()
+    for tag in rets['counts'].keys():
+        max_len = max(rets['counts'][tag].keys())
+        min_len = min(rets['counts'][tag].keys())
         print('[{}]max: {}, min: {}'.format(tag, max_len, min_len))
-        for i in range(min_len, max_len+1, 1):
-            if i in counts[tag].keys():
-                print('{}: {}'.format(i, len(counts[tag][i])))
-            else:
-                print('{}: {}'.format(i, 0))
+        with open('../data/analytic/counts-sentence_length-{}.csv'.format(tag), 'w', encoding='utf-8-sig') as f:
+            f.write('sentence length,counts\n')
+            for i in range(min_len, max_len+1, 1):
+                if i in rets['counts'][tag].keys():
+                    f.write('{},{}\n'.format(i, len(rets['counts'][tag][i])))
+                else:
+                    f.write('{},{}\n'.format(i, 0))
 
-
-def main():
-    generate_stats_files()
-
+        for label in labels[tag]:
+            max_len = max(rets['counts_by_labels'][tag][label].keys())
+            min_len = min(rets['counts_by_labels'][tag][label].keys())
+            with open('../data/analytic/counts-sentence_length-{}-{}.csv'.format(tag, label), 'w', encoding='utf-8-sig') as f:
+                f.write('sentence length,counts\n')
+                for i in range(min_len, max_len + 1, 1):
+                    if i in rets['counts_by_labels'][tag][label].keys():
+                        f.write('{},{}\n'.format(i, rets['counts_by_labels'][tag][label][i]))
+                    else:
+                        f.write('{},{}\n'.format(i, 0))
 
 
 if __name__ == '__main__':
-    main()
+    generate_stats_files()
