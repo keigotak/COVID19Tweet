a136cc8b0600d77188d4c45c7dd55327afd4b290
keigotak <keigotak@localhost.com>
2020-07-03 20:13:54 +0900

    feat: add EDA

--- src/EDA.py
+++ src/EDA.py
@@ -1,4 +1,7 @@
 from HelperFunctions import get_raw_datasets
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
 
 
 def probe_labels():
@@ -64,13 +67,13 @@ def probe_vocabs():
 
 def generate_stats_files():
     rets = probe_labels()
-    for tag in rets['counts'].keys():
-        with open('../data/analytic/counts-label-{}.csv'.format(tag), 'w', encoding='utf-8-sig') as f:
-            f.write('label,frequency\n')
+    with open('../data/analytic/counts-label.csv', 'w', encoding='utf-8-sig') as f:
+        f.write('tag,label,frequency\n')
+        for tag in rets['counts'].keys():
             for key, val in rets['counts'][tag].items():
-                f.write('{},{}\n'.format(key, val))
-    labels = rets['labels']
+                f.write('{},{},{}\n'.format(tag, key, val))
 
+    labels = ['INFORMATIVE', 'UNINFORMATIVE']
     rets = probe_vocabs()
     for tag in rets['counts'].keys():
         sorted_counts = sorted(rets['counts'][tag].items(), key=lambda x: x[1], reverse=True)
@@ -79,7 +82,7 @@ def generate_stats_files():
             for key, val in sorted_counts:
                 f.write('{},{}\n'.format(key, val))
 
-        for label in labels[tag]:
+        for label in labels:
             sorted_counts = sorted(rets['counts_by_labels'][tag][label].items(), key=lambda x: x[1], reverse=True)
             with open('../data/analytic/counts-vocab-{}-{}.csv'.format(tag, label), 'w', encoding='utf-8-sig') as f:
                 f.write('word,frequency\n')
@@ -92,24 +95,30 @@ def generate_stats_files():
         min_len = min(rets['counts'][tag].keys())
         print('[{}]max: {}, min: {}'.format(tag, max_len, min_len))
         with open('../data/analytic/counts-sentence_length-{}.csv'.format(tag), 'w', encoding='utf-8-sig') as f:
-            f.write('sentence length,counts\n')
+            f.write('sentence length,all,{}\n'.format(','.join(labels)))
             for i in range(min_len, max_len+1, 1):
+                line = []
                 if i in rets['counts'][tag].keys():
-                    f.write('{},{}\n'.format(i, len(rets['counts'][tag][i])))
+                    line.append('{},{}'.format(i, len(rets['counts'][tag][i])))
                 else:
-                    f.write('{},{}\n'.format(i, 0))
-
-        for label in labels[tag]:
-            max_len = max(rets['counts_by_labels'][tag][label].keys())
-            min_len = min(rets['counts_by_labels'][tag][label].keys())
-            with open('../data/analytic/counts-sentence_length-{}-{}.csv'.format(tag, label), 'w', encoding='utf-8-sig') as f:
-                f.write('sentence length,counts\n')
-                for i in range(min_len, max_len + 1, 1):
+                    line.append('{},{}'.format(i, 0))
+                for label in labels:
                     if i in rets['counts_by_labels'][tag][label].keys():
-                        f.write('{},{}\n'.format(i, rets['counts_by_labels'][tag][label][i]))
+                        line.append('{}'.format(rets['counts_by_labels'][tag][label][i]))
                     else:
-                        f.write('{},{}\n'.format(i, 0))
+                        line.append('{}'.format(0))
+                f.write('{}\n'.format(','.join(line)))
+
+
+def visualize_stats():
+    tags = ['train', 'valid']
+    labels = ['INFORMATIVE', 'UNINFORMATIVE']
+
+    df = pd.DataFrame.from_csv('../data/analytic/counts-label.csv', encoding='utf-8-sig').reset_index()
+    sns.barplot(x='label', y='frequency', data=df, hue='tag')
+    plt.show()
 
 
 if __name__ == '__main__':
     generate_stats_files()
+    visualize_stats()
