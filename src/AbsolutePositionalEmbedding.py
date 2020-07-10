d8fc990f9b9952ecf1b4228df7ed1769686cb671
keigotak <keigotak@localhost.com>
2020-07-10 20:01:30 +0900

    feat: add absolute position embedding

---
+++ src/AbsolutePositionalEmbedding.py
@@ -0,0 +1,32 @@
+import torch
+import torch.nn as nn
+
+from Indexer import Indexer
+
+
+class AbsolutePositionalEmbedding(nn.Module):
+    def __init__(self, device):
+        super(AbsolutePositionalEmbedding, self).__init__()
+        self.max_length = 150
+        self.indexer = Indexer(with_preprocess=False)
+        self.indexer.add_sentence(list(range(self.max_length)), with_raw=True)
+        self.embedding_dim = 20
+        self.embedding = nn.Embedding(num_embeddings=len(self.indexer), embedding_dim=self.embedding_dim, padding_idx=self.indexer.padding_index)
+        self.device = device
+        self.embedding.to(device)
+
+    def forward(self, sentences):
+        indexes = self.indexer.text_to_index(sentences)
+        pad_indexes = self.pad_sequence(indexes)
+        pad_indexes = torch.Tensor(pad_indexes).long().to(self.device)
+        vectors = self.embedding(pad_indexes)
+        return vectors
+
+    def pad_sequence(self, indexes):
+        max_len = max(map(len, indexes))
+        pad_sequences = [sentence + [self.indexer.padding_index] * (max_len - len(sentence)) if len(sentence) < max_len else sentence for sentence in indexes]
+        return pad_sequences
+
+
+if __name__ == '__main__':
+    ne = AbsolutePositionalEmbedding(device='cpu')
