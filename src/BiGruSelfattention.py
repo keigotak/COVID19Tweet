362c7b8e62bc4c9e15760e7947850e7254db4a48
romth <roomtheater@gmail.com>
2020-07-18 20:47:22 +0900

    feat: add factory class for hyperparameter search

--- src/BiGruSelfattention.py
+++ src/BiGruSelfattention.py
@@ -1,3 +1,5 @@
+import copy
+
 import torch
 import torch.nn as nn
 
@@ -11,7 +13,7 @@ from Batcher import Batcher
 from DataPooler import DataPooler
 from ValueWatcher import ValueWatcher
 from Metrics import get_metrics, get_print_keys
-from HelperFunctions import get_now
+from HelperFunctions import get_now, get_datasets
 
 
 class BiGruSelfattention(nn.Module):
@@ -26,12 +28,10 @@ class BiGruSelfattention(nn.Module):
         self.f_gru2 = nn.GRU(input_size=emb_dim, hidden_size=emb_dim, batch_first=True)
         self.b_gru2 = nn.GRU(input_size=emb_dim, hidden_size=emb_dim, batch_first=True)
 
-        self.num_head = 8
+        self.num_head = hyper_params['num_head']
         self.attention = nn.ModuleList([Attention(dimensions=emb_dim) for _ in range(self.num_head)])
 
-        self.dropout1 = nn.Dropout(0.2)
-        self.dropout2 = nn.Dropout(0.2)
-        self.dropout3 = nn.Dropout(0.2)
+        self.dropout = nn.Dropout(hyper_params['dropout_ratio'])
 
         self.pooling = nn.AdaptiveAvgPool1d(1)
         self.output = nn.Linear(emb_dim, 1)
@@ -44,23 +44,23 @@ class BiGruSelfattention(nn.Module):
         embeddings = torch.cat(embeddings, dim=2)
         max_len = embeddings.shape[1]
 
-        outf1, hidf1 = self.f_gru1(self.dropout1(embeddings))
+        outf1, hidf1 = self.f_gru1(self.dropout(embeddings))
         resf1 = outf1 + embeddings
         rev_resf1 = resf1[:,torch.arange(max_len-1, -1, -1),:] # reversed
 
-        outb1, hidb1 = self.b_gru1(self.dropout1(rev_resf1))
+        outb1, hidb1 = self.b_gru1(self.dropout(rev_resf1))
         resb1 = outb1 + rev_resf1
         rev_resb1 = resb1[:,torch.arange(max_len-1, -1, -1),:] # not reversed
 
-        outf2, hidf2 = self.f_gru2(self.dropout2(rev_resb1))
+        outf2, hidf2 = self.f_gru2(self.dropout(rev_resb1))
         resf2 = outf2 + rev_resb1
         rev_resf2 = resf2[:,torch.arange(max_len-1, -1, -1),:] # reversed
 
-        outb2, hidb2 = self.b_gru2(self.dropout2(rev_resf2))
+        outb2, hidb2 = self.b_gru2(self.dropout(rev_resf2))
         resb2 = outb2 + rev_resf2
         rev_resb2 = resb2[:,torch.arange(max_len-1, -1, -1),:] # not reversed
 
-        drop_output = self.dropout3(rev_resb2)
+        drop_output = self.dropout(rev_resb2)
         seq_logits, attention_weights = [], []
         for i in range(self.num_head):
             l, w = self.attention[i](query=drop_output, context=drop_output)
@@ -95,26 +95,65 @@ class BiGruSelfattention(nn.Module):
             return AbsolutePositionalEmbedding(device=device)
 
 
+class BaseFactory:
+    def __init__(self):
+        datasets, tags = get_datasets()
+        self.initial_train_batcher = Batcher(x=[pairs[0] for pairs in datasets['train']],
+                                             y=[pairs[1] for pairs in datasets['train']])
+        self.initial_valid_batcher = Batcher(x=[pairs[0] for pairs in datasets['valid']],
+                                             y=[pairs[1] for pairs in datasets['valid']])
+        self.criterion = nn.BCEWithLogitsLoss()
+
+
+class Factory(BaseFactory):
+    def __init__(self, device='cpu', hyper_params={}):
+        super(Factory, self).__init__()
+        self.hyper_params = self.init_hyperparameters()
+        self.hyper_params = {key: hyper_params[key] if key in hyper_params.keys() else val for key, val in self.hyper_params.items()}
+
+        self.train_batcher = copy.deepcopy(self.initial_train_batcher.set_batch_size(self.hyper_params['train_batch_size']))
+        self.valid_batcher = copy.deepcopy(self.initial_valid_batcher)
+
+        self.device = torch.device(device)
+        self.model = BiGruSelfattention(device=self.device, hyper_params=self.hyper_params)
+        print(self.model)
+
+        for parameter in self.model.parameters():
+            if not parameter.requires_grad:
+                print(parameter)
+
+        if self.hyper_params['optimizer'] == 'sgd':
+            self.optimizer = torch.optim.SGD(self.model.parameters(), lr=self.hyper_params['lr'], momentum=self.hyper_params['momentum'])
+        elif hyper_params['optimizer'] == 'adam':
+            self.optimizer = torch.optim.Adam(self.model.parameters(), lr=self.hyper_params['lr'])
+
+    @staticmethod
+    def init_hyperparameters():
+        hyper_params = {'lr': 0.01,
+                        'num_head': 8,
+                        'dropout_ratio': 0.2,
+                        'train_batch_size': 4,
+                        'weight_decay': 0.0,
+                        'clip_grad_nurm': 0.0,
+                        'optimizer': 'sgd',
+                        'momentum': 0.0
+                        }
+        return hyper_params
+
+    def generate(self):
+        return {'model': self.model,
+                'batchers': {'train': self.train_batcher, 'valid': self.valid_batcher},
+                'optimizer': self.optimizer,
+                'criterion': self.criterion
+                }
+
+
 if __name__ == '__main__':
-    from HelperFunctions import get_datasets
-    datasets, tags = get_datasets()
-    train_batcher = Batcher(x=[pairs[0] for pairs in datasets['train']], y=[pairs[1] for pairs in datasets['train']])
-    valid_batcher = Batcher(x=[pairs[0] for pairs in datasets['valid']], y=[pairs[1] for pairs in datasets['valid']])
-
-    device = torch.device('cuda:3')
-    model = BiGruSelfattention(device=device)
-    print(model)
-
-    for parameter in model.parameters():
-        if not parameter.requires_grad:
-            print(parameter)
-
-    criterion = nn.BCEWithLogitsLoss()
-    optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
-    # optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-
+    device = 'cuda:0'
+    factory = Factory(device=device).generate()
+    model, batchers, optimizer, criterion = factory['model'], factory['batchers'], factory['optimizer'], factory['criterion']
     TRAIN_MODE, VALID_MODE = 'train', 'valid'
-    batchers = {TRAIN_MODE: train_batcher, VALID_MODE: valid_batcher}
+    batchers = {TRAIN_MODE: batchers[TRAIN_MODE], VALID_MODE: batchers[VALID_MODE]}
     poolers = {VALID_MODE: DataPooler()}
     valuewatcher = ValueWatcher()
     epochs = 100
@@ -146,14 +185,14 @@ if __name__ == '__main__':
                 running_loss[mode] += loss.item()
 
                 preds = torch.sigmoid(outputs)
-                predict_label = [0 if item < 0.5 else 1 for item in preds.squeeze().tolist()]
+                predicted_label = [0 if item < 0.5 else 1 for item in preds.squeeze().tolist()]
                 poolers[mode].set('epoch{}-x'.format(e+1), x_batch)
                 poolers[mode].set('epoch{}-y'.format(e+1), y_batch)
                 poolers[mode].set('epoch{}-logits'.format(e+1), outputs.squeeze().tolist())
                 poolers[mode].set('epoch{}-preds'.format(e+1), preds.squeeze().tolist())
-                poolers[mode].set('epoch{}-predict_label'.format(e+1), predict_label)
+                poolers[mode].set('epoch{}-predicted_label'.format(e+1), predicted_label)
 
-        metrics = get_metrics(poolers[mode].get('epoch{}-predict_label'.format(e+1)), poolers[mode].get('epoch{}-y'.format(e+1)))
+        metrics = get_metrics(poolers[mode].get('epoch{}-predicted_label'.format(e+1)), poolers[mode].get('epoch{}-y'.format(e+1)))
         poolers[mode].set('epoch{}-metrics'.format(e+1), metrics)
         poolers[mode].set('epoch{}-train_loss'.format(e+1), running_loss[TRAIN_MODE])
         poolers[mode].set('epoch{}-valid_loss'.format(e+1), running_loss[VALID_MODE])
@@ -161,7 +200,7 @@ if __name__ == '__main__':
         batchers[mode].reset(with_shuffle=False)
 
         now = get_now()
-        text_line = '|'.join(['{} {}: {:.3f}'.format(mode, key, 100 * metrics[key]) if key not in set(['tp', 'fp', 'fn', 'tn']) else '{} {}: {}'.format(mode, key, metrics[key]) for key in get_print_keys() ])
+        text_line = '|'.join(['{} {}: {:.3f}'.format(mode, key, 100 * metrics[key]) if key not in set(['tp', 'fp', 'fn', 'tn']) else '{} {}: {}'.format(mode, key, metrics[key]) for key in get_print_keys()])
         print('{}|epoch: {:3d}|train loss: {:.2f}|valid loss: {:.2f}|{}'.format(now, e+1, running_loss[TRAIN_MODE], running_loss[VALID_MODE], text_line))
 
         if valuewatcher.is_over():
