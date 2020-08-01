e29eb5dadca3c395eafd18bfccf5613555a75337
keigotak <keigotak@localhost.com>
2020-08-01 19:19:45 +0900

    refactor: delete unused file

--- src/GloveTweetEmbedding.py
+++
@@ -1,7 +0,0 @@
-import gensim
-
-# Load Google's pre-trained Word2Vec model.
-model = gensim.models.KeyedVectors.load_word2vec_format('../data/models/glove.twitter.27B/glove.twitter.27B.200d.txt')
-
-print(model)
-
