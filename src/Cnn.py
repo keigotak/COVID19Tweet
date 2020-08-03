9d736b1457e4dae86a84c5619a5583a212b9d976
keigotak <keigotak@localhost.com>
2020-08-03 22:13:25 +0900

    feat: update for sequential window sliding

--- src/Cnn.py
+++ src/Cnn.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 from AbstractModel import AbstractModel
 from Attention import Attention
@@ -15,52 +16,31 @@ class Cnn(AbstractModel):
         self.kernel_size = self.hyper_params['kernel_size']
         self.window_sizes = [self.hyper_params['window_size1'], self.hyper_params['window_size2'], self.hyper_params['window_size3']]
         self.hidden_size = emb_dim
-        self.padding_sizes = [min(item // 2, 2) for item in self.window_sizes]
+        self.padding_sizes = [item // 2 for item in self.window_sizes]
         self.input_chs = [1] * 3
         self.output_chs = [self.kernel_size] * 3
 
         self.dropout = nn.Dropout(hyper_params['dropout_ratio'])
-        self.cnns = nn.ModuleList([nn.Conv1d(in_channels=self.hidden_size, out_channels=och, kernel_size=(ws,), stride=1, padding=pd) for ich, och, ws, pd in zip(self.input_chs, self.output_chs, self.window_sizes, self.padding_sizes)])
+        self.cnns = nn.ModuleList([nn.Conv2d(in_channels=ich, out_channels=och, kernel_size=(ws, emb_dim), stride=1, padding=(pd, 0)) for ich, och, ws, pd in zip(self.input_chs, self.output_chs, self.window_sizes, self.padding_sizes)])
 
-        self.num_head = hyper_params['num_head']
-        self.num_layer = hyper_params['num_layer']
-        self.attentions = nn.ModuleList([nn.MultiheadAttention(embed_dim=self.kernel_size, num_heads=self.num_head, dropout=hyper_params['dropout_ratio']) for _ in range(self.num_layer)])
-        self.linears = nn.ModuleList([nn.Linear(self.kernel_size, self.kernel_size) for _ in range(self.num_layer)])
-
-        self.output = nn.Linear(self.hidden_size * self.hidden_size, 1)
+        self.output = nn.Linear(sum(self.output_chs), 1)
 
         self.to(device)
 
     def forward(self, batch_sentence):
         embeddings = [embedding(batch_sentence) for embedding in self.embeddings]
         embeddings = torch.cat(embeddings, dim=2)
-        batch_size = embeddings.shape[0]
+        seq_len = embeddings.shape[1]
 
-        feature_map = [torch.relu(layer(self.dropout(embeddings.transpose(2, 1)))) for layer in self.cnns]
-        l = torch.cat(feature_map, dim=2)
-        l = l.transpose(2, 1)
+        feature_maps = [torch.relu(layer(self.dropout(embeddings.unsqueeze(1)))) for layer in self.cnns]
+        feature_maps = [F.max_pool1d(feature_map.squeeze(3), kernel_size=seq_len).squeeze(2) for feature_map in feature_maps]
+        drop_output = self.dropout(torch.cat(feature_maps, dim=1))
 
-        seq_logits, attention_weights = [], []
-        for i in range(self.num_layer):
-            atten_l, w = self.attentions[i](query=l, key=l, value=l)
-            l = l + atten_l
-            l = torch.layer_norm(l, normalized_shape=(l.shape[1], l.shape[2]))
-            fc_l = self.linears[i](l)
-            l = l + fc_l
-            l = torch.layer_norm(l, normalized_shape=(l.shape[1], l.shape[2]))
-            seq_logits.append(l)
-            attention_weights.append(w)
-
-        refined_seq = seq_logits[-1].view(batch_size, -1).clone()
-        if refined_seq.shape[1] > self.output.in_features:
-            refined_seq = refined_seq.narrow(1, 0, self.output.in_features)
-        elif refined_seq.shape[1] < self.output.in_features:
-            refined_seq = torch.cat((refined_seq, torch.zeros(batch_size, self.output.in_features - refined_seq.shape[1])), dim=1)
-
-        output = self.output(refined_seq)
+        output = self.output(drop_output)
         return output
 
 
 '''
+2020.08.03 22:07:13|epoch:   7|train loss: 322.05|valid loss: 149.15|valid f1: 79.838|valid precision: 76.505|valid recall: 83.475|valid accuracy: 80.100|valid tp: 394|valid fp: 121|valid fn: 78|valid tn: 407
 '''
 
